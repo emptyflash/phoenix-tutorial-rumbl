e32e7073a21dfe29dfe3575a85e72fe3eb25f345
Cameron Alexander <emptyflash@gmail.com>
2016-04-17 17:33:14 -0500

    Added authentication, log in, log out and views

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,6 +2,19 @@ defmodule Rumbl.UserController do
   alias Rumbl.User
   use Rumbl.Web, :controller
 
+  plug :authenticate when action in [:index, :show]
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to view that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
+  end
+
   def index(conn, _params) do
     users = Repo.all(User)
     render conn, "index.html", users: users
@@ -18,10 +31,11 @@ defmodule Rumbl.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
+    changeset = User.registration_changeset(%User{}, user_params)
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
